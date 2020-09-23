000827fc305d768f4deda147c2a94815bb76e353
Kingsley <kingsley.chilin@gmail.com>
2020-09-23 23:29:15 +0800

    修改ControlVector

--- ControlVector.py
+++ ControlVector.py
@@ -12,7 +12,7 @@ def ControlVector_Webster(LenLink, TotalTimeStep, SignalControl, Time_SignalPeri
     control = np.ones((LenLink, TotalTimeStep))  # Initialization
 
     for sig in SignalControl:
-        ControlVector = np.array([]).reshape(-1, 2) # Initialize the control vector of each signal
+        ControlVector = np.array([]).reshape(-1, 2)  # Initialize the control vector of each signal
 
         for j in range(sig.GreenSplit.shape[0]):
             CycleTime = sum(sig.GreenSplit[j, :]) + 2 * LostTime  # Calculate Cycle time
@@ -22,25 +22,27 @@ def ControlVector_Webster(LenLink, TotalTimeStep, SignalControl, Time_SignalPeri
             Cycle[sig.GreenSplit[j, 0] + LostTime:sig.GreenSplit[j, 0] + LostTime + sig.GreenSplit[j, 1], 1] = 1  # Second stage with green
 
             # Add the thread in the head of signal as offsets
-            OffSetSignal = np.tile(Cycle, (4, 1))  # Use 4 cycles to be the thread of the offset
+            # OffSetSignal = np.tile(Cycle, (4, 1))  # Use 4 cycles to be the thread of the offset
 
             Cycle_Num = Time_SignalPeriod[j] // CycleTime + 1  # Calculate the number of cycles
 
             CycleList = np.tile(Cycle, (Cycle_Num, 1))  # Record the overall signal of each demand level
             # CycleList = np.hstack([OffSetSignal, np.tile(Cycle, (Cycle_Num, 1))])  # Record the overall signal of each demand level
 
-            StartTime = OffSetSignal.shape[0] - sig.Offset  # determine the start time including the offset period
+            StartTime = CycleTime - sig.Offset % CycleTime  # determine the start time including the offset period
+            # StartTime = OffSetSignal.shape[0] - sig.Offset  # determine the start time including the offset period
             EndTime = StartTime + Time_SignalPeriod[j]
 
-            FinalSignal = CycleList[StartTime:EndTime, :]
-
-            ControlVector = np.vstack([ControlVector, FinalSignal])  # Integrate the offset thread
+            ControlVector = np.vstack([ControlVector, CycleList[StartTime:EndTime, :]])  # Integrate the offset thread
 
         control[sig.Restricted[0], :] = ControlVector[:, 0]
         control[sig.Restricted[1], :] = ControlVector[:, 1]
+
+    return control
 
 
 SignalControl = []
 SignalControl.append(us.signal(8, 43, [14, 20], [24, 35, 56, 82, 24, 35]))
 SignalControl.append(us.signal(9, 93, [15, 20], [25, 34, 59, 79, 25, 34]))
-ControlVector_Webster(21, 300, SignalControl, [900, 1800, 900], 5)
+control = ControlVector_Webster(21, 3600, SignalControl, [900, 1800, 900], 5)
+np.savetxt('result.csv', control, delimiter=',')
